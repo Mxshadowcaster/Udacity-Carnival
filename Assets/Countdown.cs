93ffab00ac1913ca334be185ae5671da5a34bb1c
Mxshadowcaster <Jaedlambert97@gmail.com>
2019-01-16 21:18:23 -0500

    First Commit

---
+++ Assets/Countdown.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngineUI;
+using System.Threading;
+
+public class Countdown : MonoBehaviour {
+
+public int timeLeft =60; //Seconds Overall
+public int Text countdown; //UI Text Object
+
+	void Start () {
+                StartCoroutine("LoseTime");
+                Time.timeScale = 1; //Ensures Timescale is right
+	}
+	
+	
+	void Update () {
+		countdown.text = ("" + timeLeft); //Showing score on canvas
+	}
+
+        //Simple Coroutine
+        IEnumerator LoseTime () {
+                while (true) {
+                        yield return new WaitForSeconds (1);
+                        timeLeft--;
+                }
+        }
+}
